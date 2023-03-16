67f3fd9559a0357de99e2dcb92dd61e66ec3873d
Rawllings <113606952+Rawllings@users.noreply.github.com>
2023-03-16 21:46:38 +0300

    Changes

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+config.hosts << "rails-production-09fd.up.railway.app"
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
